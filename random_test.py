8d96819de27f26eb8b7dc734afe303293fbfa8e2
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-03-15 19:10:22 -0400

    changes random test thing to use VideoRecorder instead of Monitor

--- random_test.py
+++ random_test.py
@@ -1,26 +1,33 @@
 import gym
+gym.logger.set_level(gym.logger.DEBUG)
 import numpy as np
 from gym.wrappers.monitor import Monitor
 import fetch_block_construction
+from gym.wrappers.monitoring import video_recorder
+
 env = gym.make('FetchBlockConstruction_2Blocks_SparseReward_DictstateObs_42Rendersize_FalseStackonly_SingletowerCase-v1')
-env = Monitor(env, directory="videos", force=True, video_callable=lambda x: x)
-
+# env = Monitor(env, directory="videos", force=True, video_callable=lambda x: x)
+vid = video_recorder.VideoRecorder(env,path="./videos/vid.mp4")
 env.env._max_episode_steps = 50
-# env.env.seed(0)
 
-env.reset()
+# import ipdb; ipdb.set_trace()
+
+obs = env.reset()
 env.env.stack_only = True
 
 step=0
 while True:
-    obs, done =env.reset(), False
-    while not done:
-        # env.render()
+    for i in range(50):
+        env.render()
+        vid.capture_frame()
         action = np.asarray([0, 0, 0, 0])
         step_results = env.step(action)
         obs, reward, done, info = step_results
         print("Reward: {} Info: {}".format(reward, info))
         if done:
             step = 0
+            env.reset()
+            vid.close()
+            break
         step+=1
         print(step)
