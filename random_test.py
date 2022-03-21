8bb9d3cf4755173cef6d081883436a5cf64284b4
Willie McClinton <wbm3@athena.localdomain>
2022-03-20 23:40:32 -0400

    Added on-table predicate classifier

--- random_test.py
+++ random_test.py
@@ -13,6 +13,25 @@ env.env._max_episode_steps = 50
 obs = env.reset()
 env.env.stack_only = True
 
+def on_table(env, obj):
+    # Get table z-coordinate
+    table_dim = [0.25, 0.35, 0.2]
+    table_id = env.sim.model.body_name2id('table0')
+    table_pos = env.sim.model.body_pos[table_id]
+    table_pos_min = table_pos - table_dim
+    table_pos_max = table_pos + table_dim
+    table_z = table_pos_max[-1]
+    
+    # Get object z-coordinate
+    object_height = 0.025
+    object_pos = env.sim.data.get_site_xpos(obj)
+    object_base_z = object_pos[-1] - object_height
+    
+    if (table_pos_min[0] <= object_pos[0] <= table_pos_max[0] and table_pos_min[1] <= object_pos[1] <= table_pos_max[1] and abs(table_z - object_base_z) <= 1e-3):
+        return True
+    else:
+        return False
+
 step=0
 while True:
     for i in range(50):
@@ -20,6 +39,8 @@ while True:
         # vid.capture_frame()
         action = np.asarray([0, 0, 0, 0])
         step_results = env.step(action)
+        print(on_table(env, 'object0'))
+        print(on_table(env, 'object1'))
         obs, reward, done, info = step_results
         print("Reward: {} Info: {}".format(reward, info))
         if done:
